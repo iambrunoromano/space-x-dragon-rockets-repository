3126f26dbbb3160579cd4cac46d14c5b5e1d7803
Bruno Romano <brunoromano@hotmail.it>
2025-11-15 12:53:24 +0100

    tracking rockets on mission + typo fix

--- src/main/java/org/example/entity/Mission.java
+++ src/main/java/org/example/entity/Mission.java
@@ -1,19 +1,31 @@
 package java.org.example.entity;
 
-import org.example.enums.MissionStatusEnum;
+import java.org.example.enums.MissionStatusEnum;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mission {
-  private MissionStatusEnum MissionStatusEnum;
+  private MissionStatusEnum missionStatusEnum;
+  private List<Rocket> rocketList;
 
   public Mission(MissionStatusEnum missionStatusEnum) {
-    MissionStatusEnum = missionStatusEnum;
+    this.missionStatusEnum = missionStatusEnum;
+    this.rocketList = new ArrayList<>();
   }
 
   public MissionStatusEnum getMissionStatusEnum() {
-    return MissionStatusEnum;
+    return this.missionStatusEnum;
   }
 
   public void setMissionStatusEnum(MissionStatusEnum missionStatusEnum) {
-    MissionStatusEnum = missionStatusEnum;
+    this.missionStatusEnum = missionStatusEnum;
+  }
+
+  public List<Rocket> getRockets() {
+    return this.rocketList;
+  }
+
+  public void setRockets(List<Rocket> rocketList) {
+    this.rocketList = rocketList;
   }
 }

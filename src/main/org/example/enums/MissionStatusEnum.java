6f6ddbe0f90e5702b8553bdebcafc2f0e45842eb
Bruno Romano <brunoromano@hotmail.it>
2025-11-15 14:26:24 +0100

    notes

--- src/main/org/example/enums/MissionStatusEnum.java
+++ src/main/org/example/enums/MissionStatusEnum.java
@@ -4,7 +4,11 @@ public enum MissionStatusEnum {
   SCHEDULED("Scheduled"),
   PENDING("Pending"),
   IN_PROGRESS("In Progress"),
-  ENDED("Ended");
+  ENDED(
+      "Ended"); // I point here out how there is no way to make rockets land and therefore end the
+                // mission. I saw that the ENDED status should be assigned when the mission has no
+                // rocket assigned anymore, but it is out of scope of the library methods to make
+                // rockets land and mission end (no methods ddescribed for that).
 
   private String value;
 
